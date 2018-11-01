c744d9bad18b8d08df81060749c3e04e115bc91d
Petri Airio <petri.j.airio@gmail.com>
2018-11-01 11:22:48 +0200

    Do not use @PostConstruct

--- src/main/java/org/freakz/hokan_ng_springboot/bot/common/service/HokanModuleServiceImpl.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/common/service/HokanModuleServiceImpl.java
@@ -5,10 +5,10 @@ import org.freakz.hokan_ng_springboot.bot.common.jpa.entity.PropertyEntity;
 import org.freakz.hokan_ng_springboot.bot.common.jpa.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.Date;
 
 /**
@@ -16,7 +16,7 @@ import java.util.Date;
  */
 @Component
 @Scope("singleton")
-public class HokanModuleServiceImpl implements HokanModuleService {
+public class HokanModuleServiceImpl implements HokanModuleService, CommandLineRunner {
 
     @Autowired
     private PropertyService propertyService;
@@ -28,7 +28,7 @@ public class HokanModuleServiceImpl implements HokanModuleService {
 
     private HokanModule module;
 
-    @PostConstruct
+//    @PostConstruct
     public void setHokanModule() {
         this.module = HokanModule.valueOf(hokanModule);
         this.sessionId = new Date().getTime();
@@ -50,4 +50,8 @@ public class HokanModuleServiceImpl implements HokanModuleService {
         return this.sessionId;
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        setHokanModule();
+    }
 }
