6f9fa7c1ec5d6142cb4b2fe79d6ad15f045e2ed9
Petri Airio <ext_petri.airio@samlink.fi>
2018-12-03 16:00:24 +0200

    Move JPA stuff to own artifact

--- src/main/java/org/freakz/hokan_ng_springboot/bot/common/service/StatsServiceImpl.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/common/service/StatsServiceImpl.java
@@ -1,16 +1,9 @@
 package org.freakz.hokan_ng_springboot.bot.common.service;
 
-import org.freakz.hokan_ng_springboot.bot.common.jpa.entity.IrcLog;
-import org.freakz.hokan_ng_springboot.bot.common.models.StatsData;
 import org.freakz.hokan_ng_springboot.bot.common.models.StatsMapper;
-import org.freakz.hokan_ng_springboot.bot.common.util.StringStuff;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.GregorianCalendar;
-import java.util.List;
 
 /**
  * Created by Petri Airio on 21.8.2015.
@@ -20,7 +13,7 @@ public class StatsServiceImpl implements StatsService {
 
     @Override
     public StatsMapper getDailyStatsForChannel(LocalDateTime day, String channel) {
-        List<IrcLog> logs = null; // TODO ircLogService.findByTimeStampBetweenAndTargetContaining(TimeUtil.getStartAndEndTimeForDay(day), channel);
+/*        List<IrcLog> logs = null; // TODO ircLogService.findByTimeStampBetweenAndTargetContaining(TimeUtil.getStartAndEndTimeForDay(day), channel);
         StatsMapper statsMapper = new StatsMapper();
         if (logs.size() == 0) {
             ZonedDateTime zdt = ZonedDateTime.of(day, ZoneId.systemDefault());
@@ -30,28 +23,21 @@ public class StatsServiceImpl implements StatsService {
                     + channel);
         } else {
             processLogs(statsMapper, logs);
-        }
+        }*/
+        StatsMapper statsMapper = new StatsMapper();
         return statsMapper;
     }
 
-    private void processLogs(StatsMapper statsMapper, List<IrcLog> logsForDay) {
-        for (IrcLog ircLog : logsForDay) {
-            String sender = ircLog.getSender();
-            String[] words = ircLog.getMessage().split(" ");
-            StatsData statsData = statsMapper.getStatsDataForNick(sender);
-            statsData.addToLines(1);
-            statsData.addToWords(words.length);
-        }
-    }
 
     public StatsMapper getStatsForChannel(String channel) {
-        List<IrcLog> logs = null; // TODO ircLogService.findByTarget(channel);
+/*        List<IrcLog> logs = null; // TODO ircLogService.findByTarget(channel);
         StatsMapper statsMapper = new StatsMapper();
         if (logs.size() == 0) {
             statsMapper.setError("No stats for channel: " + channel);
         } else {
             processLogs(statsMapper, logs);
-        }
+        }*/
+        StatsMapper statsMapper = new StatsMapper();
         return statsMapper;
     }
 }
